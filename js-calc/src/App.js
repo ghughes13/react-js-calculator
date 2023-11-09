2702d72d74eb563d2c6d003b613b2f8fd2de881f
ghughes139 <battletank130@gmail.com>
2023-11-09 16:40:57 -0600

    Pushing local changes so I can delete this project locally

--- js-calc/src/App.js
+++ js-calc/src/App.js
@@ -1,19 +1,59 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    let renderArea = document.querySelector(".render-calculation");
+    let elements = document.querySelectorAll(".calculator div p");
+    elements = Array.from(elements);
+    elements.shift();
+    let calculationHolder = [];
+    let finalCalc = 0;
+    let operator = "";
+
+    renderArea.innerText = finalCalc;
+
+    elements.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        if (e.target.parentElement.classList.contains("num")) {
+          calculationHolder.push(e.target.innerText);
+          renderArea.innerText = calculationHolder.join("");
+        } else if (e.target.parentElement.classList.contains("operator")) {
+          finalCalc = parseInt(calculationHolder.join(""));
+          calculationHolder = [];
+          operator = e.target.innerText;
+        } else if (e.target.parentElement.classList.contains("equal")) {
+          console.log(finalCalc);
+          console.log(operator);
+          console.log(calculationHolder.join(""));
+          renderArea.innerText = eval(
+            finalCalc + operator + calculationHolder.join("")
+          );
+        } else if (e.target.parentElement.classList.contains("clear")) {
+          calculationHolder = [];
+          finalCalc = 0;
+          operator = "";
+          renderArea.innerText = 0;
+        } else if (e.target.parentElement.classList.contains("back")) {
+          calculationHolder.pop();
+          renderArea.innerText = calculationHolder.join("");
+        }
+      });
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <div class="calculator">
           <div class="view">
-          <p class="render-calculation">0</p>
+            <p class="render-calculation">0</p>
           </div>
           <div class="clear func">
             <p>C</p>
           </div>
           <div class="back func">
-            <p>{'<--'}</p>
+            <p>{"<--"}</p>
           </div>
           <div class="divide operator">
             <p>\</p>
@@ -67,4 +107,3 @@ function App() {
 }
 
 export default App;
-
